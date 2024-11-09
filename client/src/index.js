d6c0564fce19281814c3ad74c12ff02998fd161c
lav02468 <lav02468@uga.edu>
2024-11-09 11:28:48 -0500

    added component folder, routes, and models

--- client/src/index.js
+++ client/src/index.js
@@ -2,4 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 import App from './App';
-const root = ReactDOM.createRoot(root.render(<App />), document.getElementById('root'));
+
+// Create the root
+const rootElement = document.getElementById('root');
+const root = ReactDOM.createRoot(rootElement); // Declare root after rootElement
+
+// Render the App component
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
--- server/index.js
+++ server/index.js
@@ -2,16 +2,21 @@ import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
 import cors from 'cors';
+import postsRoutes from './routes/posts.js';
 
 const app = express();
+app.use('/posts', postsRoutes);
 
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: "30mb", extended: true}));
+app.use(bodyParser.urlencoded({limit: "30mb", extended: true}));
 app.use(cors());
-app.get('/', (req, res) => {
-    res.send('App');
-});
-// Start server 
+
+const connection_url = 'mongodb+srv://javascript:javascript123@cluster0.clz2m.mongodb.net/Cluster0?retryWrites=true&w=majority';
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-console.log('Server running on port ${PORT}');
-});
+
+// Remove useUnifiedTopology and useNewUrlParser
+mongoose.connect(connection_url)
+    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
+    .catch((error) => console.log(error.message));
+
+    
